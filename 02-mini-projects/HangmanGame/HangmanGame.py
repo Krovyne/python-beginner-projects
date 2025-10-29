d37f08ba94211103f04819e347f50d2b8c535724
Vee <krovyne.vee@gmail.com>
2025-10-29 21:42:59 +0800

    update hangman

--- 02-mini-projects/HangmanGame/HangmanGame.py
+++ 02-mini-projects/HangmanGame/HangmanGame.py
@@ -7,14 +7,27 @@ word_list = [
     "train", "ball", "light", "rain", "wind", "snow", "fire", "sand", "beach", "music",
     "happy", "smile", "sleep", "walk", "run", "play", "jump", "sing", "read", "write"
 ]
+display = ""
+placeholder = ""
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
- 
-user_guess = input("Guess the letter in the word = ").lower()
-for word in chosen_word: # You can loop through Strings the same way you can loop through Lists(no need to change into list first!)
-    #string is a set of word strung together like list(or we call it in sequence)!
-    if user_guess == word:
-        print("Right")
-    else:
-        print("Wrong")
+
+letters = len(chosen_word)
+#changing the word into underscores
+for count in range(letters):
+    placeholder += "_"
+print(placeholder)
+
+while display != chosen_word:
+    user_guess = input("Guess the letter in the word = ").lower()
+
+    for letter in chosen_word: # You can loop through Strings the same way you can loop through Lists(no need to change into list first!)
+        #string is a set of word strung together like list(or we call it in sequence)!
+        
+        if user_guess == letter:
+            display += user_guess
+        else:
+            display += "_"
+    
+    print(display)
